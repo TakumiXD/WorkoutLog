673106e539e10ce1b02b44f77057d4e217890d2b
tsugita26 <85015271+tsugita26@users.noreply.github.com>
2022-09-24 17:55:15 -0700

    Update settings.py and urls.py

--- WorkoutLogger/settings.py
+++ WorkoutLogger/settings.py
@@ -133,6 +133,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
